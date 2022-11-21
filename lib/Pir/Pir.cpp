033d65a016ef45b45e0291e9d4657c74f90ae59a
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-11-21 13:43:44 +0100

    Change read() return type to bool

--- lib/Pir/Pir.cpp
+++ lib/Pir/Pir.cpp
@@ -2,11 +2,11 @@
 
 Pir::Pir(const int pin) : Sensor(pin) { Pir::time = millis(); };
 
-float Pir::read() {
+bool Pir::read() {
   if (!calibrated) {
     if (millis() - time < CALIBRATION_TIME) {
       return 0.0;
     }
   }
-  return digitalRead(pin) == HIGH ? 0.0 : 1.0;
+  return digitalRead(pin) == HIGH ? true : false;
 }
--- lib/Pir/Pir.h
+++ lib/Pir/Pir.h
@@ -7,13 +7,12 @@
 #define CALIBRATION_TIME 100000 // milliseconds
 
 class Pir : Sensor {
-  const int pin;
   long unsigned time = 0.0;
   bool calibrated = false;
   
   public:
     Pir(const int pin); 
-    float read();
+    bool read();
 };
 
 #endif
