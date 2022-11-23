aabe99e35fabdd3a98ac192f24595263f7a04a48
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2022-11-23 13:57:22 +0100

    Add Const

--- lib/Photoresistor/Photoresistor.cpp
+++ lib/Photoresistor/Photoresistor.cpp
@@ -4,8 +4,8 @@ Photoresistor::Photoresistor(const int pin) : Sensor(pin) {}
 
 int Photoresistor::read() {
   int value = analogRead(pin);
-  float voltage = value * (5.0 / 1023.0);
-  /*int val = map(value, 0, 1023, 0, 255);
+  /*float voltage = value * (5.0 / 1023.0);
+  int val = map(value, 0, 1023, 0, 255);
   analogWrite(11, val);*/
   return value;
 }
