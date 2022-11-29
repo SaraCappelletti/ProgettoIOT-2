58a8e9d48e1d57c74a6e8dadb4d8a684ead8e911
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2022-11-29 18:18:24 +0100

    Add checkbox and slider

--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@ from serial import Serial
 from time import sleep
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.widgets import Slider, CheckButtons
 
 print("Waiting Arduino for rebooting...")
 sleep(1)
@@ -15,8 +16,41 @@ def read():
     return float(data[0]), float(data[1])
 
 plt.ion()
-plt.plot([], [])
-for _ in range(1000):
+fig, ax = plt.subplots()
+
+fig.subplots_adjust(bottom=0.3)
+axfreq = fig.add_axes([0.25, 0.05, 0.65, 0.03])
+freq_slider = Slider(
+    ax=axfreq,
+    label='Degrees ',
+    valmin=0,
+    valmax=180,
+    valstep=1,
+)
+def on_slider(value):
+    print(value)
+freq_slider.on_changed(on_slider)
+
+rax = plt.axes([0.1, 0.12, 0.5, 0.1])
+check = CheckButtons(rax, ['Take control of the valve'], [False])
+
+def update():
+    #x = xdata[-1]+1
+    #xdata.append(x)
+    #ydata.append(np.sin(x/10))
+    y, x = read()
+    xvett.append(x)
+    yvett.append(y)
+
+    ax.plot(xvett, yvett, 'b')
+    plt.pause(0.00001)
+    
+plt.show()
+
+while plt.get_fignums():
+    update()
+
+'''while True:
     y, x = read()
     xvett.append(x)
     yvett.append(y)
@@ -26,13 +60,14 @@ for _ in range(1000):
     plt.plot(xvett, yvett)
     plt.draw()
     plt.pause(0.01)
-    if(len(xvett) > 150) :
+    if(len(xvett) > 100) :
         xvett = xvett[1:]
         yvett = yvett[1:]
+    myplot()
 
 input()
 
 # https://pygal.org/en/stable/
 # https://matplotlib.org/
 # https://seaborn.pydata.org/
-# https://bokeh.org/
+# https://bokeh.org/'''
