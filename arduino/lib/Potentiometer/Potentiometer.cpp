5a1913104c4f1e09aa7813eed69632d2afe98d4c
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2022-11-29 13:03:53 +0100

    Add potentiometer sensibility

--- arduino/lib/Potentiometer/Potentiometer.cpp
+++ arduino/lib/Potentiometer/Potentiometer.cpp
@@ -3,5 +3,10 @@
 Potentiometer::Potentiometer(const int pin) : Component(pin) {}
 
 int Potentiometer::read() {
-  return map(analogRead(pin), 0, 1024, 0 , 180);
+  int value = map(analogRead(pin), 0, 1024, 0 , 180);
+  if(abs(prev - value) > POT_SENSIBILITY){
+    prev = value;
+    return value;
+  }
+  return prev;
 }
